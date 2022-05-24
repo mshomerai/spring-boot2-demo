5f90717d51507943a127164b755ca9d0f81633b6
216 <216@git.com>
2022-05-24 16:43:51 +0800

    v1.0.1

---
+++ src/main/java/ml/java/boot2/SpringBoot2Application.java
@@ -0,0 +1,14 @@
+package ml.java.boot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//boot项目启动主程序
+@SpringBootApplication
+public class SpringBoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot2Application.class, args);
+    }
+
+}
---
+++ src/main/java/ml/java/boot2/rests/Rest1.java
@@ -0,0 +1,14 @@
+package ml.java.boot2.rests;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Rest1 {
+
+    @RequestMapping("/helloWorld")
+    String helloWorld() {
+        return "Hello World!";
+    }
+
+}
